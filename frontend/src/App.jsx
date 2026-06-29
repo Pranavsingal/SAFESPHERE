819014601b2cf156d16ec905f03160dec04a875f
Kriti <imkritisinha@gmail.com>
2026-06-29 11:21:49 +0530

    Wire RiskMap to real backend data via worker-zone mapping

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-import CCTVPanel from './components/CCTVPanel';
+import RiskMap from './pages/RiskMap';
 
 function App() {
-  return <CCTVPanel />;
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg1NDdhMjBjLWEyODQtNGU5Yi1hMGRhLWUxYzUzYTAwOTZkZSIsInVzZXJuYW1lIjoia3JpdGkiLCJuYW1lIjoiS3JpdGkiLCJpYXQiOjE3ODI2Mzg3ODksImV4cCI6MTc4NTIzMDc4OX0.ezm8EAlLZ1stOTZObXrkJKZa061u_I7NRKmGi7-Xhik";
+  return <RiskMap token={token} />;
 }
 
 export default App;
